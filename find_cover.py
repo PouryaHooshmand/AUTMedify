dd12dc28f7a56d2f087ee9cdf23711a0e7efc1f2
Miladosos <norooziosos@gmail.com>
2018-05-25 23:23:29 +0430

    Change Return now returning downloaded cover location

--- find_cover.py
+++ find_cover.py
@@ -4,4 +4,5 @@ from google_images_download import google_images_download
 def get_cover(album, artist):
     response = google_images_download.googleimagesdownload()
     args = {"keywords": album + " " + artist, "limit": 1, "print_urls": True, "specific_site": "wikipedia.org"}
-    print(response.download(args))
+    result = response.download(args)
+    return result[args["keywords"]][0]
