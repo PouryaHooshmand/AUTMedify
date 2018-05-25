134eabb4beda0b8fc3ff4550c7b59fdc19015524
Miladosos <norooziosos@gmail.com>
2018-05-25 21:17:50 +0430

    Removed Search_directory function

--- Recognizer.py
+++ Recognizer.py
@@ -1,23 +1,9 @@
 import json
-import os
 
 from acrcloud.recognizer import ACRCloudRecognizer
 
 from Dictionary_Difference import DictDiffer
 from Music_metadata import MusicMetadata
-
-
-def Search_directory(search_dir):
-    music_list = []
-
-    # search in given directory
-    for root, dirs, files in os.walk(search_dir):
-        for file in files:
-            # list of file types
-            if file.endswith(".mp3") or file.endswith(".m4a"):
-                music_list.append({'address': root, 'name': file})
-
-    return music_list
 
 
 class Recognizer:
