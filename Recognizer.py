a8ff7b8594ae240b15e8eb8192571adf6c39c8cf
Miladosos <norooziosos@gmail.com>
2018-05-26 22:41:40 +0430

    Let's log something

--- Recognizer.py
+++ Recognizer.py
@@ -3,12 +3,16 @@ import json
 from acrcloud.recognizer import ACRCloudRecognizer
 
 from Dictionary_Difference import DictDiffer
+from Logger import Logger
 from Music_metadata import MusicMetadata
 
 
 class Recognizer:
 
     def __init__(self):
+
+        self.log = Logger().log
+
         # Loading Necessary Information from Config File
         with open("./config/config.json") as config_file:
             config_data = json.load(config_file)
@@ -18,6 +22,7 @@ class Recognizer:
                                     "access_secret": config_data["ACRCloud"]["access_secret"].encode('utf8'),
                                     "timeout": config_data["ACRCloud"]["timeout"]}
 
+        self.log('info', 'initializing connection to ACR-Cloud service')
         self.acrcloud = ACRCloudRecognizer(self.ACRCloud_config)
 
     def recognize(self, track_location):
@@ -29,17 +34,16 @@ class Recognizer:
         metainfo = self.get_metainfo(track_location)
 
         if metainfo["status"] != "Success":
+            self.log('error', track_location + ' metadata not found')
             return False
 
         self.edit_metadata(track_location, metainfo)
-
+        self.log('info', track_location + ' recognized!\tACR-Cloud results:\n' + str(metainfo))
         return True
-
-        # TODO: Log Something
 
     def edit_metadata(self, track_location, new_metadata):
         # TODO: CHECK THE FUCKKING TEST.PY
-        # FUUUUUUUUUUCK THIS THIS.
+        # FUUUUUUUUUUCK THIS SHIT.
         # CHECK THE FUCKING STRING
         # CHECK THE FUCKING Genre OBJEECT
         # FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK
@@ -54,8 +58,11 @@ class Recognizer:
     def get_metainfo(self, track_location):
 
         metainfo = self.get_acrcloud_metainfo(track_location)
+
         if metainfo["status"]["code"] == 3000:
+            self.log('error', 'Unknown Error happend for track : ' + track_location + '\nACR-Cloud results:\n' + str(metainfo))
             return metainfo
+
         else:
             return {"album": metainfo["metadata"]["music"][0]["album"]["name"],
                     "album_artist": metainfo["metadata"]["music"][0]["artists"][0]["name"],
@@ -68,4 +75,5 @@ class Recognizer:
                     }
 
     def get_acrcloud_metainfo(self, track_location):
+        self.log('info', 'requesting from ACR-Cloud: ' + track_location)
         return json.loads(self.acrcloud.recognize_by_file(track_location, 20))
--- Search_directory.py
+++ Search_directory.py
@@ -1,16 +1,22 @@
 import os
-import json
 
+from Logger import Logger
 
 def search_directory(search_dir):
+    log = Logger().log
     music_list = []
-
     # search in given directory
+    log('info', 'searching directory to find music')
     for root, dirs, files in os.walk(search_dir):
         for file in files:
             # list of file types
             if file.endswith(".mp3") or file.endswith(".m4a"):
                 music_list.append({'address': root, 'name': file})
+
+    if music_list:
+        log('info', 'searching complete\nmusic files: ' + str(music_list))
+    else:
+        log('info', 'empty directory. nothing to search')
 
     return music_list
 
