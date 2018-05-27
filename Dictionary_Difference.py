a74c9f57712bf95aae8cc1a41267cb91b8b9894d
Miladosos <norooziosos@gmail.com>
2018-05-27 08:26:39 +0430

    Changed Returning results
    "added function" : returning a dict from items added
    "removed function" : returning a dict from items removed
    "changed function" : returning a dict from keys same in both but changed values
    "unchanged function" : returning a dict from keys same in both and unchanged values

--- Dictionary_Difference.py
+++ Dictionary_Difference.py
@@ -9,18 +9,37 @@ class DictDiffer(object):
 
     def __init__(self, current_dict, past_dict):
         self.current_dict, self.past_dict = current_dict, past_dict
-        self.set_current, self.set_past = set(current_dict.keys()), set(past_dict.keys())
-        self.intersect = self.set_current.intersection(self.set_past)
+        self.set_current_keys, self.set_past_keys = set(current_dict.keys()), set(past_dict.keys())
+        self.intersect = self.set_current_keys.intersection(self.set_past_keys)
 
     def added(self):
-        return self.set_current - self.intersect
+        added_keys = list(self.set_current - self.intersect)
+        result = {}
+        for key in added_keys:
+            result[key] = self.current_dict[key]
+
+        return result
 
     def removed(self):
-        return self.set_past - self.intersect
+        removed_keys = list(self.set_past - self.intersect)
+        result = {}
+        for key in removed_keys:
+            result[key] = self.current_dict[key]
+
+        return result
 
     def changed(self):
-        return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
+        result = {}
+        for key in list(self.intersect):
+            if self.current_dict[key] != self.past_dict[key]:
+                result[key] = self.current_dict[key]
+
+        return result
 
     def unchanged(self):
-        return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
+        result = {}
+        for key in list(self.intersect):
+            if self.current_dict[key] == self.past_dict[key]:
+                result[key] = self.current_dict[key]
 
+        return result
