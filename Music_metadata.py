614cd5307622d1560ac160c88e3138b534a3a6de
Miladosos <norooziosos@gmail.com>
2018-05-31 17:04:01 +0430

    Made BIG changes
    changed main library from eyed3 to stagger (requires Python +3)
    added cover changing ability
    added lyric embedding ability

--- Music_metadata.py
+++ Music_metadata.py
@@ -1,5 +1,6 @@
 import eyed3
-import eyed3.plugins.genres
+
+import stagger
 
 __version__ = 0.1
 
@@ -9,132 +10,75 @@ class MusicMetadata:
         """
         :param music_address: Music File address
         """
-
-        self.audiofile = eyed3.load(music_address)
+        self.music_address = music_address
+        self.musicfile = stagger.read_tag(music_address)
 
     def metadata(self):
-        return {'album': self.audiofile.tag.album,
-                'album_artist': self.audiofile.tag.album_artist,
-                'album_type': self.audiofile.tag.album_type,
-                'artist': self.audiofile.tag.artist,
-                'artist_origin': self.audiofile.tag.artist_origin,
-                'artist_url': self.audiofile.tag.artist_url,
-                'audio_file_url': self.audiofile.tag.audio_file_url,
-                'audio_source_url': self.audiofile.tag.audio_source_url,
-                'bpm': self.audiofile.tag.bpm,
-                'cd_id': self.audiofile.tag.cd_id,
-                'copyright_url': self.audiofile.tag.copyright_url,
-                'disc_num': self.audiofile.tag.disc_num,
-                'genre': str(self.audiofile.tag.genre),
-                'publisher': self.audiofile.tag.publisher,
-                'release_date': self.audiofile.tag.release_date,
-                'title': self.audiofile.tag.title,
-                'track_num': self.audiofile.tag.track_num
+        return {'album': self.musicfile.album,
+                'album_artist': self.musicfile.album_artist,
+                'artist': self.musicfile.artist,
+                'disc_num': self.musicfile.disc,
+                'genre': self.musicfile.genre,
+                'release_date': self.musicfile.date,
+                'title': self.musicfile.title,
+                'track_num': self.musicfile.track
                 }
 
     def metadata_setter(self, data):
 
         try:
-            self.audiofile.tag.album = data['album']
+            self.musicfile.album = data['album'].decode('utf-8')
         except:
             pass
 
         try:
-            self.audiofile.tag.album_artist = data['album_artist']
+            self.musicfile.album_artist = data['album_artist'].decode('utf-8')
         except:
             pass
 
         try:
-            self.audiofile.tag.album_type = data['album_type']
+            self.musicfile.artist = data['artist'].decode('utf-8')
         except:
             pass
 
         try:
-            self.audiofile.tag.artist = data['artist']
+            self.musicfile.disc = data['disc_num'].decode('utf-8')
         except:
             pass
 
         try:
-            self.audiofile.tag.artist_origin = data['artist_origin']
+            self.musicfile.genre = data['genre'].decode('utf-8')
+        except:
+            pass
+
+        try: # TODO: COVER
+            self.musicfile.picture = data['image']
         except:
             pass
 
         try:
-            self.audiofile.tag.artist_url = data['artist_url']
+            self.musicfile.date = data['release_date'].decode('utf-8')
         except:
             pass
 
         try:
-            self.audiofile.tag.audio_file_url = data['audio_file_url']
+            self.musicfile.title = data['title'].decode('utf-8')
         except:
             pass
 
         try:
-            self.audiofile.tag.audio_source_url = data['audio_source_url']
+            self.musicfile.track = data['track_num'].decode('utf-8')
         except:
             pass
 
         try:
-            self.audiofile.tag.bpm = data['bpm']
+            if data['lyric']:
+                track = eyed3.load(self.music_address)
+                track.tag.lyrics.set(data['lyric'].decode('utf-8'))
+                track.tag.save()
         except:
             pass
 
-        try:
-            self.audiofile.tag.cd_id = data['cd_id']
-        except:
-            pass
-
-        try:
-            self.audiofile.tag.copyright_url = data['copyright_url']
-        except:
-            pass
-
-        try:
-            self.audiofile.tag.disc_num = data['disc_num']
-        except:
-            pass
-
-        try:
-            self.audiofile.tag.genre = data['genre']
-        except:
-            pass
-
-        try:
-            self.audiofile.tag.images = data['images']
-        except:
-            pass
-
-        try:
-            self.audiofile.tag.publisher = data['publisher']
-        except:
-            pass
-
-        try:
-            self.audiofile.tag.release_date = data['release_date']
-        except:
-            pass
-
-        try:
-            self.audiofile.tag.title = data['title']
-        except:
-            pass
-
-        try:
-            self.audiofile.tag.track_num = data['track_num']
-        except:
-            pass
-
-        self.audiofile.tag.save()
+        self.musicfile.write()
 
         return True
-
-    def rename(self):
-        new_name = self.audiofile.tag.track_num + ". " + self.audiofile.tag.title
-
-        try:
-            self.audiofile.rename(new_name)
-            return True
-        except:
-            return False
-
-        
