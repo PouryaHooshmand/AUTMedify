c3bf8e926952399262d1c54b0f94207cf587b90c
pourya76h <hooshi.pourya@gmail.com>
2018-06-01 16:05:19 +0430

    fixed bugs
    used MusicMetadata class
    added logger
    get address instead of album and artist name

--- find_cover.py
+++ find_cover.py
@@ -1,8 +1,26 @@
 from google_images_download import google_images_download
+from Logger import Logger
+from Music_metadata import MusicMetadata
 
 
-def get_cover(album, artist):
+
+def get_cover(address):
+    log = Logger().log
+    MD = MusicMetadata(address).metadata()
+    album = MD["album"]
+    artist = MD["artist"]
+
+    log('info', 'Downloading ' + album + " " + artist + " cover")
+
     response = google_images_download.googleimagesdownload()
+
     args = {"keywords": album + " " + artist, "limit": 1, "print_urls": True, "specific_site": "wikipedia.org"}
-    result = response.download(args)
+    try:
+        result = response.download(args)
+    except:
+        log('error', 'Unknown error happened while downloading ' + str(album) + " " + str(artist) + " cover")
+        return 0
+
+    log('info', 'Download ' + str(album) + " " + str(artist) + " cover COMPLETED")
+
     return result[args["keywords"]][0]
