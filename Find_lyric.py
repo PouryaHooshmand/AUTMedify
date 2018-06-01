c52c7020e2ac18ae08ca453bb935a48ee2076336
Miladosos <norooziosos@gmail.com>
2018-06-01 23:04:23 +0430

    bugs fixes changing lyric

--- Find_lyric.py
+++ Find_lyric.py
@@ -1,15 +1,25 @@
 import swagger_client as LyricAPI
 
-from swagger_client.rest import ApiException
+
+from Music_metadata import MusicMetadata
 
 LyricAPI.configuration.api_key['apikey'] = 'd58b51bad4512088ae210ba3f06c2909'
 
 
-def findLyrics(title, artist):
+def findLyrics(track_location):
     api_instance = LyricAPI.LyricsApi()
+
+    track = MusicMetadata(track_location)
+
+    track_metadata = track.metadata()
+
+    title = track_metadata['title']
+    artist = track_metadata['artist']
 
     Lyric_obj = api_instance.matcher_lyrics_get_get(q_track=title, q_artist=artist, format='json')
 
     Lyric = Lyric_obj.message.body.lyrics.lyrics_body
 
-    return Lyric
+    track.metadata_setter({'lyric': Lyric})
+
+    return 1
